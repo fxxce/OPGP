ce90ad8b970f425937be2c9bb49052d046550f52
fxce <69138642+fxxce@users.noreply.github.com>
2023-08-13 06:26:45 -0700

    Updated

--- users/registration.php
+++ users/registration.php
@@ -119,7 +119,7 @@ function reCaptcha($recaptcha){
 	<footer style="position: fixed; margin-bottom: 0%; bottom: 0; left: 0; background-color: black; padding: 10px; color: grey; width: 100%; margin-top: 10px; text-align: center; border-top: rgb(175, 45, 28) 3px solid; font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif">
             <div class="row">
                 <div class="col-lg-12">
-                    <p style="padding-top: 2px;">Copyright &copy; <a>kmbhasan</a> | Public Grievance Portal 2020</p>
+                    <p style="padding-top: 2px;">Copyright &copy; <a>fxxce</a> | Public Grievance Portal 2020</p>
                 </div>
             </div>
 	</footer>
